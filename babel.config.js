9105a5516944582b94c4f863694874128eacb229
six-standard <dbrrl1127@gmail.com>
2024-05-18 00:41:55 +0900

    init: 프로젝트 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [
+          ".ios.ts",
+          ".android.ts",
+          ".ts",
+          ".ios.tsx",
+          ".android.tsx",
+          ".tsx",
+          ".jsx",
+          ".js",
+          ".json",
+        ],
+        alias: {
+          "@": "./src",
+          "@utils": "./src/utils",
+          "@commons": "./src/components/common",
+        },
+      },
+    ],
+  ],
 };
